2c6c45b4c2938cebacd72e97ce197455f04a061f
PumpkinKing93 <tgillette@mail.fresnostate.edu>
2019-02-01 18:44:46 -0800

    basic working boards page

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -6,20 +6,17 @@ export default ({
     canMoveRight,
     onMoveLeft,
     onMoveRight
-}) => ( <
-    div className = "card" > {
+}) => ( <div className = "card" > {
         canMoveLeft && < button onClick = {
             onMoveLeft
         } > {
             '<'
-        } < /button>} <
-        span className = "title" > {
+        } </button>} <span className = "title" > {
             card.name
-        } < /span> {
+        } </span> {
         canMoveRight && < button onClick = {
             onMoveRight
         } > {
             '>'
-        } < /button>} < /
-        div >
+        } </button>} </div>
     )
--- src/Component/Column.js
+++ src/Component/Column.js
@@ -1,45 +1,26 @@
-import React from "react"
-import Card from "./Card"
+import React from 'react'
+import Card from './Card'
 
 export default ({
-    column,
-    columnIndex,
-    onMoveRight,
-    onMoveLeft,
-    onAddCard
-}) => ( <
-    div className = "column" >
-    <
-    h1 > {
-        column.name
-    } < /h1> {
-    column.cards.map((card, cardIndex) => ( <
-        Card key = {
-            cardIndex
-        }
-        card = {
-            card
-        }
-        cardIndex = {
-            cardIndex
-        }
-        canMoveLeft = {
-            columnIndex !== 0
-        }
-        canMoveRight = {
-            columnIndex !== 2
-        }
-        onMoveLeft = {
-            () => onMoveLeft(cardIndex)
-        }
-        onMoveRight = {
-            () => onMoveRight(cardIndex)
-        }
-        />
-    ))
-} <
-button onClick = {
-        onAddCard
-    } > + < /button> < /
-    div >
+  column,
+  columnIndex,
+  onMoveRight,
+  onMoveLeft,
+  onAddCard
+}) => (
+  <div className='column'>
+    <h1> {column.name} </h1>{' '}
+    {column.cards.map((card, cardIndex) => (
+      <Card
+        key={cardIndex}
+        card={card}
+        cardIndex={cardIndex}
+        canMoveLeft={columnIndex !== 0}
+        canMoveRight={columnIndex !== 2}
+        onMoveLeft={() => onMoveLeft(cardIndex)}
+        onMoveRight={() => onMoveRight(cardIndex)}
+      />
+    ))}{' '}
+    <button onClick={onAddCard}> + </button>{' '}
+  </div>
 )
