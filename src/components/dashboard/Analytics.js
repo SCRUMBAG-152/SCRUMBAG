1b847d1cf54fdc797dcd6f8b316045a68c3aac05
Thien Nguyen <Thiennguyenwsb@gmail.com>
2019-04-09 11:58:11 -0700

    added functionalities to Dashboard Page

---
+++ src/components/dashboard/Analytics.js
@@ -0,0 +1,116 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+// react plugin for creating charts
+import ChartistGraph from 'react-chartist'
+// react plugin for creating vector maps
+
+// @material-ui/core components
+import withStyles from '@material-ui/core/styles/withStyles'
+import Icon from '@material-ui/core/Icon'
+import Refresh from '@material-ui/icons/Refresh'
+import Edit from '@material-ui/icons/Edit'
+import Tooltip from '@material-ui/core/Tooltip'
+
+
+// @material-ui/icons
+import DateRange from '@material-ui/icons/DateRange'
+import LocalOffer from '@material-ui/icons/LocalOffer'
+import Update from '@material-ui/icons/Update'
+import ArrowUpward from '@material-ui/icons/ArrowUpward'
+import AccessTime from '@material-ui/icons/AccessTime'
+
+// core components
+import GridContainer from '../../customs/components/Grid/GridContainer.jsx'
+import GridItem from '../../customs/components/Grid/GridItem.jsx'
+import Card from '../../customs/components/Card/Card.jsx'
+import CardHeader from '../../customs/components/Card/CardHeader.jsx'
+import CardIcon from '../../customs/components/Card/CardIcon.jsx'
+import CardBody from '../../customs/components/Card/CardBody.jsx'
+import CardFooter from '../../customs/components/Card/CardFooter.jsx'
+import Button from '../../customs/components/CustomButtons/Button.jsx'
+
+
+import dashboardStyle from './dashboardStyle/dashboardStyle'
+
+const Analytics = (props) => {
+    const { classes } = props
+  return (
+    <GridContainer>
+        <GridItem xs={12} sm={6}>
+        <Card>
+            <CardHeader color='warning' stats icon>
+            <CardIcon color='warning'>
+                <Icon>content_copy</Icon>
+            </CardIcon>
+            <p className={classes.cardCategory}>Tasks Completed </p>
+            <h3 className={classes.cardTitle}>35/50</h3>
+            </CardHeader>
+            <CardFooter stats>
+            <div className={classes.stats}>
+                <DateRange />
+                Last 24 Hours
+            </div>
+            </CardFooter>
+        </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6}>
+        <Card>
+            <CardHeader color='success' stats icon>
+            <CardIcon color='success'>
+                <Icon>group</Icon>
+            </CardIcon>
+            <p className={classes.cardCategory}>Active Members</p>
+            <h3 className={classes.cardTitle}>7/10</h3>
+            </CardHeader>
+            <CardFooter stats>
+            <div className={classes.stats}>
+                <DateRange />
+                Last 24 Hours
+            </div>
+            </CardFooter>
+        </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6}>
+        <Card>
+            <CardHeader color='danger' stats icon>
+            <CardIcon color='danger'>
+                <Icon>info_outline</Icon>
+            </CardIcon>
+            <p className={classes.cardCategory}>Fixed Issues</p>
+            <h3 className={classes.cardTitle}>75</h3>
+            </CardHeader>
+            <CardFooter stats>
+            <div className={classes.stats}>
+                <LocalOffer />
+                Tracked from Github
+            </div>
+            </CardFooter>
+        </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6}>
+        <Card>
+            <CardHeader color='info' stats icon>
+            <CardIcon color='info'>
+                <i className='fab fa-twitter' />
+            </CardIcon>
+            <p className={classes.cardCategory}>Followers</p>
+            <h3 className={classes.cardTitle}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+            <div className={classes.stats}>
+                <Update />
+                Just Updated
+            </div>
+            </CardFooter>
+        </Card>
+        </GridItem>
+  </GridContainer>
+  )
+}
+
+Analytics.propTypes = {
+    classes: PropTypes.object.isRequired
+  }
+
+export default withStyles(dashboardStyle)(Analytics)
--- src/components/dashboard/Panels.js
+++ src/components/dashboard/Panels.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Notifications from './Notifications';
 
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-
+import Analytics from './Analytics'
+import Daily from './Daily'
 
 import Info from "@material-ui/icons/Info";
 import LocationOn from "@material-ui/icons/LocationOn";
-import Gavel from "@material-ui/icons/Gavel";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 import HelpOutline from "@material-ui/icons/HelpOutline";
+import Forum from "@material-ui/icons/Forum";
+import chartIcon from "@material-ui/icons/Timeline"
+
 
 // core components
 import GridContainer from "../../customs/components/Grid/GridContainer.jsx";
@@ -34,11 +39,11 @@ const styles = {
 
 class Panels extends React.Component {
   render() {
-    const { classes } = this.props;
+    const { classes, notifications } = this.props;
     return (
       <div>
         <GridContainer justify="center">
-          <GridItem xs={12} sm={12} md={8}>
+          <GridItem xs={12} sm={12} md={9}>
             <h3 className={classes.pageSubcategoriesTitle}>
               Scrumbag
             </h3>
@@ -48,30 +53,19 @@ class Panels extends React.Component {
               alignCenter
               tabs={[
                 {
-                  tabButton: "Description",
+                  tabButton: "Analytics",
                   tabIcon: Info,
                   tabContent: (
-                    <Card>
-                      <CardHeader>
-                        <h4 className={classes.cardTitle}>
-                          Description about product
-                        </h4>
-                        <p className={classes.cardCategory}>
-                          More information here
-                        </p>
-                      </CardHeader>
-                      <CardBody>
-                        Collaboratively administrate empowered markets via
-                        plug-and-play networks. Dynamically procrastinate B2C
-                        users after installed base benefits.
-                        <br />
-                        <br />
-                        Dramatically visualize customer directed convergence
-                        without revolutionary ROI.
-                      </CardBody>
-                    </Card>
+                      <Analytics/>
                   )
                 },
+                {
+                    tabButton: "Daily",
+                    tabIcon: chartIcon,
+                    tabContent: (
+                        <Daily/>
+                    )
+                  },
                 {
                   tabButton: "Location",
                   tabIcon: LocationOn,
@@ -98,32 +92,36 @@ class Panels extends React.Component {
                   )
                 },
                 {
-                  tabButton: "Legal Info",
-                  tabIcon: Gavel,
+                  tabButton: "Notifications",
+                  tabIcon: NotificationsIcon,
                   tabContent: (
-                    <Card>
-                      <CardHeader>
-                        <h4 className={classes.cardTitle}>
-                          Legal info of the product
-                        </h4>
-                        <p className={classes.cardCategory}>
-                          More information here
-                        </p>
-                      </CardHeader>
-                      <CardBody>
-                        Completely synergize resource taxing relationships via
-                        premier niche markets. Professionally cultivate
-                        one-to-one customer service with robust ideas.
-                        <br />
-                        <br />
-                        Dynamically innovate resource-leveling customer service
-                        for state of the art customer service.
-                      </CardBody>
-                    </Card>
+                    <Notifications notifications={notifications} />
                   )
                 },
                 {
-                  tabButton: "Help Center",
+                    tabButton: "Discussion",
+                    tabIcon: Forum,
+                    tabContent: (
+                      <Card>
+                        <CardHeader>
+                          <h4 className={classes.cardTitle}>
+                            Disscussion Board
+                          </h4>
+                          <p className={classes.cardCategory}>
+                            Discuss with other team members or other teams
+                          </p>
+                        </CardHeader>
+                        <CardBody>
+                          This space is for future implementation of a discussion board.
+                          <br />
+                          <br />
+                          A discussion board will help provide needed communications and promote bounching of ideas between members.
+                        </CardBody>
+                      </Card>
+                    )
+                },
+                {
+                  tabButton: "Help",
                   tabIcon: HelpOutline,
                   tabContent: (
                     <Card>
