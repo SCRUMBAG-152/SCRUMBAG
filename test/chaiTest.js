fc61cfe76b2648f96f0292bafbf929923d4cd82d
PumpkinKing93 <93gymnast@gmail.com>
2019-04-29 20:56:12 -0700

    jest

--- test/chaiTest.js
+++ test/chaiTest.js
@@ -1,36 +1,20 @@
-// var assert = require('assert');
-// var expect = require('chai').expect;
-// var should = require('chai').should();
-// it('should return true if valid user id', function(){
-//       var isValid = loginController.isValidUserId('abc123')
-//       //assert.equal(isValid, true);
-//       expect(isValid).to.be.true;
-// });
-// it('should return false if invalid user id', function(){
-//       var isValid = loginController.isValidUserId('abc1234')
-//       //assert.equal(isValid, false);
-//       isValid.should.equal(false);
-// });
-
-//let's set up the data we need to pass to the login method
-
 
 const userCredentials = {
-    email: 'chop559@gmail.com', 
-    password: ""
+    email: 'tgillette@gmail.com', 
+    password: "abc123"
   }
   //now let's login the user before we run any tests
   var authenticatedUser = request.agent(app);
   before(function(done){
     authenticatedUser
-      .post('/login')
+      .post('/pages/login-page')
       .send(userCredentials)
       .end(function(err, response){
         expect(response.statusCode).to.equal(200);
-        expect('Location', '/home');
+        expect('Location', '/');
         done();
       });
   });
-  //this test says: make a POST to the /login route with the email: sponge@bob.com, password: garyTheSnail
-  //after the POST has completed, make sure the status code is 200 
-  //also make sure that the user has been directed to the /home page
+//   //this test says: make a POST to the /login route with the email: sponge@bob.com, password: garyTheSnail
+//   //after the POST has completed, make sure the status code is 200 
+//   //also make sure that the user has been directed to the /home page
