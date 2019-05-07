9a6489273af8976cb9f06b0d94fb40a1061e9ade
Austin Cain <austincain22@gmail.com>
2019-05-06 17:37:01 -0700

    fix errors

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -12,42 +12,43 @@ const styles = theme => ({
   title: {
     borderBottom: '1px solid #eee',
   },
-  divider:{
-      fontSize: 1000
+  divider: {
+    fontSize: 1000
   },
-  item:{
-      margin: 0,
-      padding:0,
+  item: {
+    margin: 0,
+    padding: 0,
   }
 });
 
 
 const CommentList = ({ comments, classes }) => {
-
- const sortedComments = comments.sort(function(a, b) {
-    a = (a.createdAt);
-    b = (b.createdAt);
-    return b>a ? -1 : b<a ? 1 : 0;
+  if (comments) {
+    const sortedComments = comments.sort(function (a, b) {
+      a = (a.createdAt);
+      b = (b.createdAt);
+      return b > a ? -1 : b < a ? 1 : 0;
     });
-  console.log(comments)
-  console.log(sortedComments)
-  return (
-    <div>  
-    <Typography variant="h5" component="h3" >
-        Comments
-    </Typography >
-      <Divider className={classes.divider} />
-      <Grid container>
-        {sortedComments && sortedComments.map(comment => {
-          return (
-            <Grid className={classes.item} item xs={8} key={comment.id}>
-              <CommentDetails comment={comment}/>
-            </Grid>
-          )
-        })}
-      </Grid>
-    </div>
-  )
+    console.log(comments)
+    console.log(sortedComments)
+    return (
+      <div>
+        <Typography variant="h5" component="h3" >
+          Comments
+      </Typography >
+        <Divider className={classes.divider} />
+        <Grid container>
+          {sortedComments && sortedComments.map(comment => {
+            return (
+              <Grid className={classes.item} item xs={8} key={comment.id}>
+                <CommentDetails comment={comment} />
+              </Grid>
+            )
+          })}
+        </Grid>
+      </div>
+    )
+  }
 }
 
 
